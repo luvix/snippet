89936c86724b99f9925f1a9844f85d29759dedf0
Luvix Basso <luvix.basso@gmail.com>
2018-12-25 03:51:01 +0900

    sudoer와 아닌 구분이 반대로 되어있길래 고침

--- py3/install-py3-apt.sh
+++ py3/install-py3-apt.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 sudo apt install build-essential checkinstall openssl
-sudo apt-get install -ylibreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+sudo apt-get install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
 wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tar.xz
 tar xvf Python-3.6.1.tar.xz
 cd Python-3.6.1
